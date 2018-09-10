7e4ac637563ecda63647d75a277b2e4d507162d0
Sanit <sanitreddy@gmail.com>
2018-09-10 11:58:45 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { HelloComponent } from './hello/hello.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'intro';
+
+  records = [
+  {
+  	name: 'Sam',
+  	online: true
+  },
+  {
+  	name: 'John',
+  	online: false
+  },
+  {
+  	name: 'Ted',
+  	online: true
+  },
+  {
+    name: 'Sam',
+    online: true
+  },
+  {
+    name: 'John',
+    online: false
+  },
+  {
+    name: 'Ted',
+    online: true
+  }
+ ]
 }
